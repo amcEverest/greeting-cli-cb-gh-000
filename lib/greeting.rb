4a951205e25a468ab227ea2c2cc1268a482c3a4a
Andres Cortez <andres.corteztm@gmail.com>
2018-10-05 21:21:08 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,6 +1,3 @@
-#!/ur/bin/env ruby
-require_relative '../lib/greeting.rb'
-
-puts "Hi! I'm HAL, what's your name?"
-name = gets.name
-greeting(name)
+def greeting(name)
+  puts "Hello #{name}. It's nice to meet you."
+end
